c602fc485e6f65ee61e434f196088d6135e6ad2b
Abhishek  Sharma <63914624+Abhisheksharma251195@users.noreply.github.com>
2021-01-02 17:53:16 +0530

    gbg

---
+++ shopRootsAdmin.core/interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using shopRootsAdmin.core.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopRootsAdmin.core.interfaces
+{
+    public interface IRepository <T> where T : modelBase
+    {
+        public Task<IList<T>> GetAll(Func<T, bool> condition = null , bool includeDeleted =  false);
+        public Task<T> GetOne(Func<T, bool> condition);
+        public Task<T> Update(T Model);
+        public Task<T> Create(T Model);
+        public Task<bool> Delete(int id);
+    }
+}
