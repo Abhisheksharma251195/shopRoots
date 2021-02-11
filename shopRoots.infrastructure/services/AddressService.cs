31696a4e60139c34b6bebf53aedb7965f3a3052f
Abhishek  Sharma <63914624+Abhisheksharma251195@users.noreply.github.com>
2021-02-11 21:16:03 +0530

    Add file

---
+++ shopRoots.infrastructure/services/AddressService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shopRootsAdmin.core.interfaces;
+using shopRootsAdmin.core.models;
+
+namespace shopRoots.infrastructure.services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IRepository<AddressModel> _addressService;
+        public AddressService(IRepository<AddressModel> addressService) {
+            _addressService = addressService;
+        }
+        public async Task<AddressModel> createAddress(AddressModel addressModel)
+        {
+            return await _addressService.Create(addressModel); 
+        }
+
+        public async Task<bool> deleteAddress(int addressId)
+        {
+            return await _addressService.Delete(addressId);
+        }
+
+        public async Task<AddressModel> getAddress(int addressId)
+        {
+            return await _addressService.GetOne(x => x.Id == addressId);
+        }
+
+        public async Task<List<AddressModel>> getAllAddress(List<int> addressIds)
+        {
+            return (List<AddressModel>)await _addressService.GetAll(x => addressIds.Contains((int)x.Id));
+        }
+
+        public async Task<AddressModel> updateAddress(AddressModel userModel)
+        {
+            return await _addressService.Update(userModel);
+        }
+    }
+}
