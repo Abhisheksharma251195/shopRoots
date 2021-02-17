4b7bd005c074932fdb9627dd1e6b7febc8977235
Abhishek  Sharma <63914624+Abhisheksharma251195@users.noreply.github.com>
2021-02-17 21:24:28 +0530

    minor changes

--- shopRootsAdmin.core/models/AddressModel.cs
+++ shopRootsAdmin.core/models/AddressModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
-
 namespace shopRootsAdmin.core.models
 {
 
@@ -13,11 +12,12 @@ namespace shopRootsAdmin.core.models
         public AddressModel()
         {
         }
-        public virtual string? AddressLine1 { get; set; }
-        public virtual string? City { get; set; }
-        public virtual string? District { get; set; }
-        public virtual string? State { get; set; }
-        public virtual string? addressType  {get; set;}
+
+        public virtual string AddressLine1 { get; set; }
+        public virtual string City { get; set; }
+        public virtual string District { get; set; }
+        public virtual string State { get; set; }
+        public virtual string addressType  {get; set;}
         public virtual int? PinCode { get; set; }
         public virtual int UserId { get; set; }
         public virtual userModel User { get; set; }
