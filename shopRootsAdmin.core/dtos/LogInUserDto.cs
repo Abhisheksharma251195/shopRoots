9a57e4949736f38e9a983243f407271626720c4a
Abhishek  Sharma <63914624+Abhisheksharma251195@users.noreply.github.com>
2021-03-01 02:24:42 +0530

    Impediment logger and auth service

---
+++ shopRootsAdmin.core/dtos/LogInUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopRootsAdmin.core.dtos
+{
+   public class LogInDto 
+    {
+        public LogInDto() { }
+        public string UserName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ shopRootsAdmin.core/dtos/UserProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopRootsAdmin.core.dtos
+{
+    public class UserProfileDto
+    {
+        public string token { get; set; }
+        public string userId { get; set; }
+        public string userName{ get; set; }
+        public string userEmail{ get; set; }
+        public string mobileNo { get; set; }
+    }
+}
